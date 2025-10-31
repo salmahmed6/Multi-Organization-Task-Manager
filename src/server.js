2d279b7a063a570246b20d2667c1f11b7f2a9e65
Salma <sa1963645@gmail.com>
2025-10-31 20:12:49 +0200

    chore: register file routes in Express app

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.routes.js";
 import organizationRoutes from "./routes/organization.routes.js";
 import projectRoutes from "./routes/project.routes.js";
 import taskRoutes from "./routes/task.routes.js";
+import fileRoutes from "./routes/file.routes.js";
 
 dotenv.config();
 const app = express();
@@ -19,6 +20,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/organizations", organizationRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
+app.use("/api/files", fileRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
