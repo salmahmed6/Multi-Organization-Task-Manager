bcb0eb62232dcf4d7c625e1f7253778432273b7a
Salma <sa1963645@gmail.com>
2025-10-31 13:51:55 +0200

    feat: add the auth middelware logic

---
+++ src/utils/jwt.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const generateToken = (user) => {
+  return jwt.sign(
+    { 
+        id: user.id,
+        role: user.role,
+        organizationId: user.organizationId
+    },
+    process.env.JWT_SECRET,
+    { 
+        expiresIn: "1d"
+    }
+  );
+};
