e32323ba28dddcf404dd8cf7788d0c965c7f60c6
Salma <sa1963645@gmail.com>
2025-10-31 21:33:44 +0200

    chore: register stats route for admin

--- src/routes/organization.routes.js
+++ src/routes/organization.routes.js
@@ -4,6 +4,7 @@ import {
   getOrganizations,
   addEmployee,
   getOrganizationById,
+  getOrganizationStats,
 } from "../controllers/organization.controller.js";
 import { verifyToken } from "../middleware/auth.middleware.js";
 import { isAdmin } from "../middleware/role.middleware.js";
@@ -15,5 +16,6 @@ router.post("/", verifyToken, isAdmin, createOrganization);
 router.get("/", verifyToken, isAdmin, getOrganizations);
 router.get("/:organizationId", verifyToken, getOrganizationById);
 router.post("/add-employee", verifyToken, isAdmin, addEmployee);
+router.get("/stats", verifyToken, isAdmin, getOrganizationStats);
 
 export default router;
