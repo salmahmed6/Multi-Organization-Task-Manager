ca2b9aa4a7eeb3ae10929262897030ab40023222
Salma <sa1963645@gmail.com>
2025-10-31 21:34:00 +0200

    chore: update task route for pagination

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -4,6 +4,7 @@ import {
   getTasks,
   updateTask,
   deleteTask,
+  getAllTasks,
 } from "../controllers/task.controller.js";
 import { verifyToken } from "../middleware/auth.middleware.js";
 import { isAdmin } from "../middleware/role.middleware.js";
@@ -14,5 +15,6 @@ router.post("/", verifyToken, isAdmin, createTask);
 router.get("/", verifyToken, getTasks);
 router.put("/:taskId", verifyToken, updateTask);
 router.delete("/:taskId", verifyToken, isAdmin, deleteTask);
+router.get("/", verifyToken, getAllTasks);
 
 export default router;
